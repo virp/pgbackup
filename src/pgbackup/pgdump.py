f38ee7f1a2c337b03ad5fef9e714452c5fc93966
Sergey Kamenskiy <skvirp@gmail.com>
2021-05-16 13:29:22 +0500

    Complete

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -1,3 +1,4 @@
+from os import times
 import subprocess
 import sys
 
@@ -8,3 +9,12 @@ def dump(url):
     except OSError as err:
         print(f"Error: {err}")
         sys.exit(1)
+
+
+def dump_file_name(url, timestamp=None):
+    db_name = url.split('/')[-1]
+    db_name = db_name.split('?')[0]
+    if timestamp:
+        return f"{db_name}-{timestamp}.sql"
+    else:
+        return f"{db_name}.sql"
